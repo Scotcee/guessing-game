c979f2f7cc2866acc43a64c8c4b6a955dfbd8920
Scotcee <66390516+Scotcee@users.noreply.github.com>
2022-10-22 05:21:26 +0000

    this is a guessing game from zuri training

--- index.js
+++ index.js
@@ -3,46 +3,91 @@
 // console.log("welcome" + " " + userName + " " + "to zuri guessing game");
 
 
-console.log(askNum());
 
 
-function askNum() {
 
 
-  return range();
-}
 
 let points = 0;
 let levels = 1;
 let maxStage = 10;
 
 
-function range() {
+// let rangeFunction = function range() {
+  
+
+  for (let i = 2; i <= 10;
+    i++
+  ) {
+    let comGuess = Math.floor(Math.random()
+      * i) + 1
+    let guessNum = parseInt(prompt(`choose a number between 1- 
+ ${i} ?`));
+
+
+
+
+    return range;
+    };
+
+
+let range = function (comAnswer, yourAnswer)
+{
+  let guessNum = yourAnswer;
+  let comGuess = comAnswer ;
+    if (comGuess === guessNum)
+    {
+      console.log ("Congratulation! Up to Stage 2")
+    }
+      
+    else if (guessNum < 1 || guessNum > i ) {
+      console.log ("invalid input try again")
+      return restart
+    }
+    
+    else if (yourAnswer !== comAnswer){
+      console.log ("you failed")
+      return restart
+    }
+    else {
+      console.log ("the end")
+      
+    }
+
+  }
+
+let restart = function range (){
   for (let i = 2; i <= 2;
     i++
   ) {
     let comGuess = Math.floor(Math.random()
       * i) + 1
-    let guessNum = parseInt(prompt(`choose a number between 1- ${i} ?`));
-    if (comGuess == guessNum) {
-
-      console.log("Congratulation! Up to Stage 2");
-      break;
-      // return askNum();
+    let guessNum = parseInt(prompt(`choose a number between 1- 
+ ${i} ?`));
+    // return guessNum;
+  
+   
     }
-    else if (guessNum == !NaN) {
-      console.log("this is not a number!")
-    }
-    else if (guessNum > comGuess) {
-      console.log("try a smaller number..")
-    }
-    else {
-      console.log("Failed Try Again");
-
-    }
-  };
 }
+  // };
+// var gameLogic = function () {
+//   if (comGuess == guessNum) {
 
+//       console.log("Congratulation! Up to Stage 2");
+//       // return askNum();
+//     }
+//     else if (guessNum == !NaN) {
+//       console.log("this is not a number!")
+//     }
+//     else if (guessNum > comGuess) {
+//       console.log("try a smaller number..")
+//     }
+//     else {
+//       console.log("Failed Try Again");
+      
+// }
+
+// }
 
 
 

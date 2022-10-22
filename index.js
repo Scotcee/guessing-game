f51a67e15c224df47dbebd5803648ecc2a1cc9ff
Scotcee <66390516+Scotcee@users.noreply.github.com>
2022-10-22 12:45:34 +0100

    Update index.js
    corrected the functions

--- index.js
+++ index.js
@@ -1,93 +1,37 @@
 
- const userName = prompt("what is your name?");
- console.log("welcome" + " " + userName + " " + "to zuri guessing game");
-
-
-
-
-
-
+const userName = prompt("what is your name?");
+console.log("welcome" + " " + userName + " " + "to zuri guessing game");
 
 let points = 0;
 let levels = 1;
 let maxStage = 10;
+let guessNum = parseInt(prompt("choose a number between 1-" + (levels + 1)));
+let comGuess = parseInt(Math.floor((Math.random())));
 
 
-// let rangeFunction = function range() {
-  
+range = function range() {
+  for (let i = 2; i <= 2; i++)
+    guessGame = function guessGame() {
+      comGuess = (((comGuess * i) + 1))
+      if (comGuess === guessNum) {
+        console.log("Congratulation Up to Stage 2")
+      }
+      else if ((guessNum < 1) || (guessNum > i) || (guessNum != Nan)) {
+        console.log("invalid input try again")
+        return guessNum;
+      }
+      else {
+        console.log("you failed")
+        return guessNum;
+      }
 
-  for (let i = 2; i <= 10;
-    i++
-  ) {
-    let comGuess = Math.floor(Math.random()
-      * i) + 1
-    let guessNum = parseInt(prompt(`choose a number between 1- 
- ${i} ?`));
-
-
-
-
-    return range;
-    };
-
-
-let range = function (comAnswer, yourAnswer)
-{
-  let guessNum = yourAnswer;
-  let comGuess = comAnswer ;
-    if (comGuess === guessNum)
-    {
-      console.log ("Congratulation! Up to Stage 2")
-    }
-      
-    else if (guessNum < 1 || guessNum > i ) {
-      console.log ("invalid input try again")
-      return restart
-    }
-    
-    else if (yourAnswer !== comAnswer){
-      console.log ("you failed")
-      return restart
-    }
-    else {
-      console.log ("the end")
-      
     }
 
-  }
-
-let restart = function range (){
-  for (let i = 2; i <= 2;
-    i++
-  ) {
-    let comGuess = Math.floor(Math.random()
-      * i) + 1
-    let guessNum = parseInt(prompt(`choose a number between 1- 
- ${i} ?`));
-    // return guessNum;
-  
-   
-    }
 }
-  // };
-// var gameLogic = function () {
-//   if (comGuess == guessNum) {
+range();
+guessGame();
 
-//       console.log("Congratulation! Up to Stage 2");
-//       // return askNum();
-//     }
-//     else if (guessNum == !NaN) {
-//       console.log("this is not a number!")
-//     }
-//     else if (guessNum > comGuess) {
-//       console.log("try a smaller number..")
-//     }
-//     else {
-//       console.log("Failed Try Again");
-      
-// }
 
-// }
 
 
 

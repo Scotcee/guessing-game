416f583fad9fcee2a278dc8bdfa75667dcc62481
Scotcee <66390516+Scotcee@users.noreply.github.com>
2022-10-22 22:59:35 +0100

    Update index.js
    i couldn't make it move from one stage to another

--- index.js
+++ index.js
@@ -1,37 +1,39 @@
-
 const userName = prompt("what is your name?");
 console.log("welcome" + " " + userName + " " + "to zuri guessing game");
+restart = function restart() {
+  var points = 0;
+  var levels = 1;
+  var maxStage = 10;
+  var guessNum = parseInt(prompt(`choose a number between (1-${levels + 1})`));
+  var comGuess = parseInt(Math.floor((Math.random())));
 
-let points = 0;
-let levels = 1;
-let maxStage = 10;
-let guessNum = parseInt(prompt("choose a number between 1-" + (levels + 1)));
-let comGuess = parseInt(Math.floor((Math.random())));
 
+  range = function range() {
+    for (let i = 2; i <= 10; i++)
+      guessGame = function guessGame() {
+        comGuess = (((comGuess * i) + 1))
+        if (comGuess === guessNum) {
+          console.log("Congratulation Up to Stage 2")
+          return restart();
+        }
+        else if ((guessNum < 1) || (guessNum > i)) {
+          console.log("invalid input try again")
+          return restart();
+        }
+        else if (isNaN(guessNum)) {
+          console.log("Only numbers are allowed")
+          return restart();
+        }
 
-range = function range() {
-  for (let i = 2; i <= 2; i++)
-    guessGame = function guessGame() {
-      comGuess = (((comGuess * i) + 1))
-      if (comGuess === guessNum) {
-        console.log("Congratulation Up to Stage 2")
+        else {
+          console.log("you failed")
+          return guessNum;
+        }
       }
-      else if ((guessNum < 1) || (guessNum > i) || (guessNum != Nan)) {
-        console.log("invalid input try again")
-        return guessNum;
-      }
-      else {
-        console.log("you failed")
-        return guessNum;
-      }
 
-    }
+  }
 
 }
+restart()
 range();
 guessGame();
-
-
-
-
-

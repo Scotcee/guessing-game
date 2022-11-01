8acb9b2704f72b6437758d4f13761479de68a561
Scotcee <66390516+Scotcee@users.noreply.github.com>
2022-11-01 22:25:14 +0100

    latest update
    added scores

--- index.js
+++ index.js
@@ -1,39 +1,30 @@
 const userName = prompt("what is your name?");
-console.log("welcome" + " " + userName + " " + "to zuri guessing game");
-restart = function restart() {
-  var points = 0;
-  var levels = 1;
-  var maxStage = 10;
-  var guessNum = parseInt(prompt(`choose a number between (1-${levels + 1})`));
-  var comGuess = parseInt(Math.floor((Math.random())));
+console.log("welcome" + " " + userName + " " + "to ZURI guessing game");
 
+var points = 0;
+var maxStage = 10;
 
-  range = function range() {
-    for (let i = 2; i <= 10; i++)
-      guessGame = function guessGame() {
-        comGuess = (((comGuess * i) + 1))
-        if (comGuess === guessNum) {
-          console.log("Congratulation Up to Stage 2")
-          return restart();
-        }
-        else if ((guessNum < 1) || (guessNum > i)) {
-          console.log("invalid input try again")
-          return restart();
-        }
-        else if (isNaN(guessNum)) {
-          console.log("Only numbers are allowed")
-          return restart();
-        }
-
-        else {
-          console.log("you failed")
-          return guessNum;
-        }
-      }
-
+function range() {
+  for (let i = 2; i <= 10; i++) {
+    let guessNum = parseInt(prompt(`choose a number between  (1-${i})`))
+    let comGuess = Math.floor((Math.random()) * i) + 1
+    if (comGuess === guessNum) {
+      console.log(`Congratulation Up to Stage ${i}`);
+    }
+    else if ((guessNum < 1) || (guessNum > i++)) {
+      console.log("invalid input try again");
+      return range()
+    }
+    else if (isNaN(guessNum)) {
+      console.log("Only numbers are allowed");
+      return range()
+    }
+    else {
+      console.log(`you failed the answer is ${comGuess}`)
+      console.log(`your score is ${i - 2}`)
+      break;
+    }
   }
 
 }
-restart()
 range();
-guessGame();
